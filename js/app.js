88067ecaac829431a219764d0a8bf988e27cc6c9
anjayluh <angella@laboremus.no>
2018-11-26 14:50:56 +0300

    Double cat clicker

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-const cat = document.querySelector('.cat')
-let count = 0;
-cat.addEventListener('click', function() {
-    count += 1;
-    document.querySelector(".count").innerText = `${count}`;
+const cats = document.querySelectorAll('.cat')
+let counterOne = 0;
+let counterTwo = 0;
+let nameOne = "Kitty";
+let nameTwo = "Daisy";
+document.querySelector(".name1").innerText = `${nameOne}`;
+document.querySelector(".name2").innerText = `${nameTwo}`;
+cats[0].addEventListener('click', function() {
+    counterOne += 1;
+    document.querySelector(".count").innerText = `${ counterOne}`;
+})
+cats[1].addEventListener('click', function() {
+    counterTwo += 1;
+    document.querySelector(".count1").innerText = `${counterTwo}`;
 })
