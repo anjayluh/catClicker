a3aa288e1a344fbdd04b769e082dd2c5876ba976
anjayluh <angella@laboremus.no>
2018-11-28 10:26:49 +0300

    Refactor solution to use MVO

--- js/app.js
+++ js/app.js
@@ -1,74 +1,83 @@
-//Get tag to display cat names
-let displayNames = document.querySelector('.selector');
-let displayImage = document.querySelector('.display');
-
 //Object containing all cats
-const allCats = {
-        cats: [{
-                clickCount: 0,
-                name: 'Daisy',
-                imgSrc: 'images/catf.jfif',
-            },
-            {
-                clickCount: 0,
-                name: 'Kitty',
-                imgSrc: 'images/kitty-cat-kitten-pet-45201.jpeg',
-            },
-            {
-                clickCount: 0,
-                name: 'Gracie',
-                imgSrc: 'images/catb.jfif',
-            },
-            {
-                clickCount: 0,
-                name: 'Smokey',
-                imgSrc: 'images/catc.jfif',
-            },
-            {
-                clickCount: 0,
-                name: 'Jack',
-                imgSrc: 'images/catd.jfif',
-            },
-            {
-                clickCount: 0,
-                name: 'Simba',
-                imgSrc: 'images/cate.jfif',
-            },
-        ],
-    }
-    //Display cat names in the list of cats to click on
-let id = 0;
-for (cat of allCats.cats) {
-    let div = document.createElement('div');
-    div.classList.add('link');
-    div.innerHTML = `<p>${allCats.cats[id].name}</p>`
-    displayNames.appendChild(div);
-    id += 1
-}
-for (const catName of displayNames.children) {
-    catName.addEventListener('click', function() {
-        //console.log(catName.children[0].innerText)
-        for (let index = 0; index < allCats.cats.length; index++) {
-            if (catName.children[0].innerText === allCats.cats[index].name) {
-                // console.log(allCats.cats[index].name);
-
-                //Add div tag and display the cat details in it
-                let div = document.createElement('div');
-                div.classList.add('image');
-                div.innerHTML = `Name: ${allCats.cats[index].name}<br/><span class="count">Count: ${allCats.cats[index].clickCount}</span><img class="image" src = "${allCats.cats[index].imgSrc}">`
-                    //add the div content to the display div in html
-                displayImage.innerHTML = div.innerHTML;
-                //Select the image tag to check for click events
-                let currentCat = document.querySelector('img');
-                //Check if image is clicked and update clickCount
-                currentCat.addEventListener('click', function() {
-                    allCats.cats[index].clickCount += 1;
-                    document.querySelector(".count").innerText = `Count: ${allCats.cats[index].clickCount}`;
-                });
-                // displayImage.appendChild(div);
-
-            }
+const data = {
+    //Create cats object to contain the cats details
+    cats: [{
+            clickCount: 0,
+            name: 'Daisy',
+            imgSrc: 'images/catf.jfif',
+        },
+        {
+            clickCount: 0,
+            name: 'Kitty',
+            imgSrc: 'images/kitty-cat-kitten-pet-45201.jpeg',
+        },
+        {
+            clickCount: 0,
+            name: 'Gracie',
+            imgSrc: 'images/catb.jfif',
+        },
+        {
+            clickCount: 0,
+            name: 'Smokey',
+            imgSrc: 'images/catc.jfif',
+        },
+        {
+            clickCount: 0,
+            name: 'Jack',
+            imgSrc: 'images/catd.jfif',
+        },
+        {
+            clickCount: 0,
+            name: 'Simba',
+            imgSrc: 'images/cate.jfif',
+        },
+    ],
+};
+octopus = {
+    id: 0,
+    //Get tags to display cat names and images
+    displayNames: document.querySelector('.selector'),
+    displayImage: document.querySelector('.display'),
+    displayCatList: function() {
+        for (cat of data.cats) {
+            let div = document.createElement('div');
+            div.classList.add('link');
+            div.innerHTML = `<p>${data.cats[this.id].name}</p>`
+            this.displayNames.appendChild(div);
+            this.id += 1
         }
+    },
+    displayClickedCat: function() {
+        for (const catName of this.displayNames.children) {
+            catName.addEventListener('click', function() {
+                //console.log(catName.children[0].innerText)
+                for (let index = 0; index < data.cats.length; index++) {
+                    if (catName.children[0].innerText === data.cats[index].name) {
+                        // console.log(allCats.cats[index].name);
 
-    });
-}
+                        //Add div tag and display the cat details in it
+                        let div = document.createElement('div');
+                        div.classList.add('image');
+                        div.innerHTML = `Name: ${data.cats[index].name}<br/><span class="count">Count: ${data.cats[index].clickCount}</span><img class="image" src = "${data.cats[index].imgSrc}">`;
+                        //add the div content to the display div in html
+                        octopus.displayImage.innerHTML = div.innerHTML;
+                        //Select the image tag to check for click events
+                        let currentCat = document.querySelector('img');
+                        //Check if image is clicked and update clickCount
+                        currentCat.addEventListener('click', function() {
+                            data.cats[index].clickCount += 1;
+                            document.querySelector(".count").innerText = `Count: ${data.cats[index].clickCount}`;
+                        });
+                        // displayImage.appendChild(div);
+
+                    }
+                }
+
+            });
+        }
+    }
+};
+//Invoke display names
+octopus.displayCatList();
+//invoke display cat
+octopus.displayClickedCat();
